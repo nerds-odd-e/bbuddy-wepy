a1c8ea20ddb8c03b52c72fa57b8fa097f7bc550f
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-02-20 13:19:22 +0800

    login: save and use token for api call

--- src/store/middleware/auth.js
+++ src/store/middleware/auth.js
@@ -1,9 +1,21 @@
 import {REQUIRE_LOGIN} from '../types'
+import wepy from 'wepy'
 
 export const auth = store => next => action => {
   if (action.payload && action.payload.statusCode === 401) {
     return next({type: REQUIRE_LOGIN})
-  } else {
-    return next(action)
   }
+
+  if (action.payload && action.payload.statusCode === 200 && action.payload.header) {
+    const {header} = action.payload
+    wepy.setStorageSync('token', {
+      'access-token': header['access-token'],
+      'token-type': header['token-type'],
+      'uid': header['uid'],
+      'client': header['client'],
+      'expiry': header['expiry']
+    })
+  }
+
+  return next(action)
 }
