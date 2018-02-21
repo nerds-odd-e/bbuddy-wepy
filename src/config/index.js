c13f5f0354bfab80afcb8e9601abc63be16faee7
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-02-21 13:20:48 +0800

    change host to ip address instead of localhost so that the mini program can be viewed in wechat

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export const env = process.env.NODE_ENV === 'production' ? 'prod' : 'dev'
 
 const hosts = {
-  dev: 'http://localhost:3000/',
+  dev: 'http://192.168.1.10:3000/',
   prod: 'https://prod.com'
 }
 
