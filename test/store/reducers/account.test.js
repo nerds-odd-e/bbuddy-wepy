ddcaac89ce224138a376e74229df868f03d90985
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-02-21 15:46:16 +0800

    make account reducer tests complete

--- test/store/reducers/account.test.js
+++ test/store/reducers/account.test.js
@@ -1,5 +1,16 @@
 import reducer from '../../../src/store/reducers/account'
+import {LOAD_ALL_ACCOUNTS} from '../../../src/store/types'
 
-test('initial state', () => {
-  expect(reducer(undefined, {type: 'ANY_ACTION'})).toEqual({allAccounts: []})
+describe('load all accounts', () => {
+  const account = {id: 1, name: 'name', balance: 100}
+  const payload = {data: [account]}
+  test('initial state not changed with other action even containing payload data', () => {
+    expect(reducer(undefined, {type: 'ANY_ACTION', payload}))
+      .toEqual({allAccounts: []})
+  })
+
+  test('initial state with action containing response payload', () => {
+    expect(reducer({allAccounts: []}, {type: LOAD_ALL_ACCOUNTS, payload}))
+      .toEqual({allAccounts: [account]})
+  })
 })
