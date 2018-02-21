3e211b104e95bf2e3450ca61938dad991506e422
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-02-21 22:40:20 +0800

    add tests for toast reducer

---
+++ test/store/reducers/toast.test.js
@@ -0,0 +1,21 @@
+import reducer from '../../../src/store/reducers/toast'
+import {ADD_ACCOUNT, DELETE_ACCOUNT, EDIT_ACCOUNT} from '../../../src/store/types'
+
+describe('account related toast', () => {
+  test('initial state', () => {
+    expect(reducer(undefined, {type: 'ANY_ACTION'}))
+      .toEqual({message: undefined})
+  })
+  test('add account action', () => {
+    expect(reducer({message: undefined}, {type: ADD_ACCOUNT}))
+      .toEqual({message: 'Add Successfully'})
+  })
+  test('edit account action', () => {
+    expect(reducer({message: undefined}, {type: EDIT_ACCOUNT}))
+      .toEqual({message: 'Edit Successfully'})
+  })
+  test('delete account action', () => {
+    expect(reducer({message: undefined}, {type: DELETE_ACCOUNT}))
+      .toEqual({message: 'Delete Successfully'})
+  })
+})
